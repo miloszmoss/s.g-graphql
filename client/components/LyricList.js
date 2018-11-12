3c1106d1f169702d4599e37292c022b7dc8dc637
miloszmoska <milosz.moss@gmail.com>
2018-11-12 14:14:15 +0100

    add likes to lyrics

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-export default class LyricList extends Component {
-  onLike(id) {
-    console.log(id);
+class LyricList extends Component {
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1,
+        },
+      },
+    });
   }
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i
-            onClick={() => this.onLike(id)}
-            style={{ color: 'rgb(59, 150, 235)', cursor: 'pointer' }}
-            className="material-icons"
-          >
-            thumb_up
-          </i>
+          <span style={{ display: 'flex', alignItems: 'center' }}>
+            <i
+              onClick={() => this.onLike(id, likes)}
+              style={{ color: 'rgb(59, 150, 235)', cursor: 'pointer', paddingRight: '10px' }}
+              className="material-icons"
+            >
+              thumb_up
+            </i>
+            <p style={{ minWidth: '20px' }}>{likes}</p>
+          </span>
         </li>
       );
     });
@@ -24,3 +39,13 @@ export default class LyricList extends Component {
     return <ul className="collection">{this.renderLyrics()}</ul>;
   }
 }
+
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+export default graphql(mutation)(LyricList);
